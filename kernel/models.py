6c408c07ce8c278f539d4c1066352498e4264ce6
pycodi <info@pycode.net>
2016-07-11 17:51:25 +0300

    update 1107
    add KernelPage

--- kernel/models.py
+++ kernel/models.py
@@ -1,17 +1,23 @@
 from django.contrib.auth.models import (AbstractBaseUser, PermissionsMixin)
 from django.core.mail import send_mail
+from django.core.cache import cache
+from django.utils.html import strip_tags
 from django.db import models
 from django.conf import settings
 from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _
 from django.utils.encoding import python_2_unicode_compatible
+from django.template.defaultfilters import truncatechars_html
 
 from polymorphic.models import PolymorphicModel
+from stdimage.models import StdImageField
+from ckeditor_uploader.fields import RichTextUploadingField
+
 
 from .constant import Lang
 from kernel.managers.user import PortalEmailUserMixinManager
 from kernel.middleware import CrequestMiddleware
-
+from kernel.utils import upload_dir, slugify
 
 from rest_framework import serializers
 
@@ -238,8 +244,67 @@ class KernelByModel(KernelModel):
         return 'id', 'external_id', 'created_by'
 
 
+class PublishedManager(models.Manager):
+    def get_query_set(self):
+        return super(PublishedManager, self).get_query_set().filter(status='published')
 
 
+@python_2_unicode_compatible
+class KernelPage(KernelByModel):
+    HIDDEN_STATUS = -1
+    DRAFT_STATUS = 0
+    PUBLISHED_STATUS = 1
+
+    STATUS_CHOICES = (
+        (HIDDEN_STATUS, 'Hidden'),
+        (DRAFT_STATUS, 'Draft'),
+        (PUBLISHED_STATUS, 'Published')
+    )
+
+    publisher = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
+    title = models.CharField(_(u'Заголовок'), max_length=255)
+    longtitle = models.CharField(_(u'Расширенный заголовок'), blank=True, max_length=255)
+    keywords = models.CharField(_(u'Ключевые слова'), blank=True, max_length=255)
+    description = models.CharField(_(u'Описание'), blank=True, max_length=255)
+    slug = models.SlugField(_('URL'), help_text=_(u'Использовать в качестве урла транскрипцию ключевых слов'), max_length=120, unique=True, blank=True)
+    image = StdImageField(upload_to=upload_dir, blank=True, variations={'promotion': (775, 275, True), 'large': (600, 400), 'thumbnail': (75, 75, True), 'medium': (300, 200)})
+    introtext = models.TextField(verbose_name=_(u'Аннотация'))
+    content = RichTextUploadingField(verbose_name=_(u'Статья'))
+
+    status = models.IntegerField(_(u'Статус'), choices=STATUS_CHOICES, default=DRAFT_STATUS)
+    comment_enabled = models.BooleanField(_('comment enabled'), default=True)
+
+    views = models.IntegerField(default=0)
+
+    objects = models.Manager()
+    published = PublishedManager()
+
+    class Meta:
+        ordering = ['-created_date']
+        abstract = True
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        cache.clear()
+        obj = super(KernelPage, self).save(*args, **kwargs)
+        return obj
+
+    @classmethod
+    def list_display(cls):
+        return 'id',  'title', 'status',
+
+    @property
+    def word_count(self):
+        return len(strip_tags(self.content))
+
+    @property
+    def get_content(self):
+        return self.content
+
+    @property
+    def get_introtext(self):
+        return truncatechars_html(self.content, 220)
 
 
 
--- kernel/utils.py
+++ kernel/utils.py
@@ -6,3 +6,7 @@ __author__ = 'pyCode'
 
 def slugify(str):
     return aw_slugify(str.lower())
+
+
+def upload_dir(instance, filename):
+    return '{1}/{0}'.format(filename, str(instance.__class__.__name__).lower())
