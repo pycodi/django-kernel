3c908d7532594836a0607194e6d064cd98e46bec
pycodi <info@pycode.net>
2016-07-28 09:48:47 +0300

    add login and logout
    add login and logout

--- kernel/urls.py
+++ kernel/urls.py
@@ -3,4 +3,10 @@ try:
 except ImportError:  # django < 1.4
     from django.conf.urls.defaults import *
 
-# place app url patterns here
+
+from kernel import views as kv
+
+urlpatterns = [
+    url(r'^login/$', kv.LoginView.as_view(), name='login'),
+   url(r'^logout/$', kv.LogoutView.as_view(), name='logout'),
+]
---
+++ kernel/views.py
@@ -0,0 +1,28 @@
+from django.contrib.auth import get_user_model
+from django.contrib import auth
+from django.views.generic import CreateView, TemplateView, FormView, RedirectView
+from django.contrib.auth.forms import (AuthenticationForm, SetPasswordForm,
+                                       PasswordChangeForm, PasswordResetForm)
+from braces.forms import UserKwargModelFormMixin
+
+
+class LoginView(FormView):
+    form_class = AuthenticationForm
+    template_name = "kernel/login.html"
+    success_url = '/'
+
+    def form_valid(self, form):
+        auth.login(self.request, form.get_user())
+        return super(LoginView, self).form_valid(form)
+
+
+class LogoutView(TemplateView, RedirectView):
+    url = '/login/'
+    permanent = True
+
+    def get(self, *args, **kwargs):
+        auth.logout(self.request)
+        if self.get_redirect_url(**kwargs):
+            return RedirectView.get(self, *args, **kwargs)
+        else:
+            return TemplateView.get(self, *args, **kwargs)
